7c2a4f8db1c322f95089a198bcf9259816950d72
Jimmy Conner <cigamit@cacti.net>
2011-02-17 22:39:13 +0000

    Check only enabled schedules
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/maint/trunk@1539 67214365-cc29-0410-bfbe-843132f0f3d7

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function plugin_maint_check_webseer_url ($host) {
 }
 
 function plugin_maint_check_host ($type, $host) {
-	$schedules = db_fetch_assoc("SELECT * FROM plugin_maint_hosts WHERE TYPE = $type AND (host = $host OR $host = 0)");
+	$schedules = db_fetch_assoc("SELECT * FROM plugin_maint_hosts WHERE enabled = 'on' AND TYPE = $type AND (host = $host OR $host = 0)");
 	if (!empty($schedules)) {
 		foreach ($schedules as $s) {
 			if (plugin_maint_check_schedule ($s['schedule'])) {
