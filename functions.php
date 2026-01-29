6dd79fd4b1c65345236dd374ce0c9c8f928aadd3
Sean Mancini <sean@seanmancini.com>
2026-01-29 12:03:31 -0500

    more cs fix changes (#41)
    * more cs fix changes
    
    * add typing and doc blocks
    
    * fix php stand paths
    
    * Update maint.php
    
    * use cacti php stand and csfixer configs
    
    * Update composer.json
    
    * Update composer.json
    
    * make sure to use the core cacti folder
    
    * Update composer.json
    
    * Update plugin-ci-workflow.yml
    
    * only scan maint plugin folder
    
    * .
    
    * use proper php stan
    
    this sould fix it !!
    
    * Update composer.json
    
    * Update composer.json

--- functions.php
+++ functions.php
@@ -24,92 +24,134 @@
  +-------------------------------------------------------------------------+
 */
 
-function plugin_maint_check_cacti_host($host)
-{
-    return plugin_maint_check_host(1, $host);
+/**
+ * Check if a Cacti host is in maintenance
+ *
+ * This is called via the 'is_device_in_maintenance' hook.
+ *
+ * @param int $host Host ID to check
+ *
+ * @return bool True if host is in active maintenance, false otherwise
+ */
+function plugin_maint_check_cacti_host(int $host): bool {
+	return plugin_maint_check_host(1, $host);
 }
 
-function plugin_maint_check_webseer_url($host)
-{
-    return plugin_maint_check_host(2, $host);
+/**
+ * Check if a WebSeer URL is in maintenance
+ *
+ * @param int $host WebSeer URL ID to check
+ *
+ * @return bool True if URL is in active maintenance, false otherwise
+ */
+function plugin_maint_check_webseer_url(int $host): bool {
+	return plugin_maint_check_host(2, $host);
 }
 
-function plugin_maint_check_servcheck_test($host)
-{
-    return plugin_maint_check_host(3, $host);
+/**
+ * Check if a Servcheck test is in maintenance
+ *
+ * @param int $host Servcheck test ID to check
+ *
+ * @return bool True if test is in active maintenance, false otherwise
+ */
+function plugin_maint_check_servcheck_test(int $host): bool {
+	return plugin_maint_check_host(3, $host);
 }
 
-
-function plugin_maint_check_host($type, $host)
-{
-    $schedules = db_fetch_assoc_prepared(
-        'SELECT *
+/**
+ * Check if a host is in maintenance based on type
+ *
+ * @param int $type Host type (1=Cacti host, 2=WebSeer URL, 3=Servcheck test)
+ * @param int $host Host/URL/Test ID to check
+ *
+ * @return bool True if host is in active maintenance schedule, false otherwise
+ */
+function plugin_maint_check_host(int $type, int $host): bool {
+	$schedules = db_fetch_assoc_prepared(
+		'SELECT *
 		FROM plugin_maint_hosts
 		WHERE TYPE = ?
 		AND (host = ? OR host = 0)',
-        [$type, $host],
-    );
+		[$type, $host],
+	);
 
-    if (!empty($schedules)) {
-        foreach ($schedules as $s) {
-            if (plugin_maint_check_schedule($s['schedule'])) {
-                return true;
-            }
-        }
-    }
-    return false;
+	if (!empty($schedules)) {
+		foreach ($schedules as $s) {
+			if (plugin_maint_check_schedule($s['schedule'])) {
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
-function plugin_maint_check_schedule($schedule)
-{
-    $sc = db_fetch_row_prepared(
-        'SELECT *
+/**
+ * Check if a maintenance schedule is currently active
+ *
+ * Handles both one-time and recurring schedules.
+ * For recurring schedules that have passed, automatically calculates
+ * and updates the next occurrence.
+ *
+ * @param int $schedule Schedule ID to check
+ *
+ * @return bool True if schedule is active now, false otherwise
+ */
+function plugin_maint_check_schedule(int $schedule): bool {
+	$sc = db_fetch_row_prepared(
+		'SELECT *
 		FROM plugin_maint_schedules
 		WHERE enabled = \'on\' AND id = ?',
-        [$schedule],
-    );
+		[$schedule],
+	);
 
-    if (!empty($sc)) {
-        $t = time();
-        switch ($sc['mtype']) {
-            case 1:
-                if ($t > $sc['stime'] && $t < $sc['etime']) {
-                    return true;
-                }
-                break;
+	if (!empty($sc)) {
+		$t = time();
 
-            case 2: // Recurring
-                /* past, calculate next */
-                if ($sc['etime'] < $t) {
-                    /* convert start and end to local so that hour stays same for add days across daylight saving time change */
-                    $starttimelocal = (new DateTime('@' . strval($sc['stime'])))->setTimezone(new DateTimeZone(date_default_timezone_get()));
-                    $endtimelocal   = (new DateTime('@' . strval($sc['etime'])))->setTimezone(new DateTimeZone(date_default_timezone_get()));
-                    $nowtime        = new DateTime();
-                    /* add interval days */
-                    $addday = new DateInterval('P' . strval($sc['minterval'] / 86400) . 'D');
-                    while ($endtimelocal < $nowtime) {
-                        $starttimelocal = $starttimelocal->add($addday);
-                        $endtimelocal   = $endtimelocal->add($addday);
-                    }
+		switch ($sc['mtype']) {
+			case 1:
+				if ($t > $sc['stime'] && $t < $sc['etime']) {
+					return true;
+				}
 
-                    $sc['stime'] = $starttimelocal->getTimestamp();
-                    $sc['etime'] = $endtimelocal->getTimestamp();
-                    /* save next interval so not need to recalculate */
-                    db_execute_prepared(
-                        'UPDATE plugin_maint_schedules
+				break;
+			case 2: // Recurring
+				// past, calculate next
+				if ($sc['etime'] < $t) {
+					// convert start and end to local so that hour stays same for add days across daylight saving time change
+					$starttimelocal = (new DateTime('@' . strval($sc['stime'])))->setTimezone(new DateTimeZone(date_default_timezone_get()));
+					$endtimelocal   = (new DateTime('@' . strval($sc['etime'])))->setTimezone(new DateTimeZone(date_default_timezone_get()));
+					$nowtime        = new DateTime();
+					// add interval days
+					$addday = new DateInterval('P' . strval($sc['minterval'] / 86400) . 'D');
+
+					while ($endtimelocal < $nowtime) {
+						$starttimelocal = $starttimelocal->add($addday);
+						$endtimelocal   = $endtimelocal->add($addday);
+					}
+
+					$sc['stime'] = $starttimelocal->getTimestamp();
+					$sc['etime'] = $endtimelocal->getTimestamp();
+					// save next interval so not need to recalculate
+					db_execute_prepared(
+						'UPDATE plugin_maint_schedules
 						SET stime = ?, etime = ?
 						WHERE id = ?',
-                        [$sc['stime'], $sc['etime'], $schedule],
-                    );
-                    /* format yyyy-mm-dd hh:mm */
-                    cacti_log('INFO: Maintenance schedule "' . $sc['name'] . '" Next start ' . $starttimelocal->format('Y-m-d H:i')
-                               . ' End ' . $endtimelocal->format('Y-m-d H:i'), false, 'MAINT');
-                }
-                if ($t > $sc['stime'] && $t < $sc['etime']) {
-                    return true;
-                }
-                break;
-        }
-    }
-    return false;
+						[$sc['stime'], $sc['etime'], $schedule],
+					);
+					// format yyyy-mm-dd hh:mm
+					cacti_log('INFO: Maintenance schedule "' . $sc['name'] . '" Next start ' . $starttimelocal->format('Y-m-d H:i')
+							   . ' End ' . $endtimelocal->format('Y-m-d H:i'), false, 'MAINT');
+				}
+
+				if ($t > $sc['stime'] && $t < $sc['etime']) {
+					return true;
+				}
+
+				break;
+		}
+	}
+
+	return false;
 }
